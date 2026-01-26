ccbc26962234376eaedec804c0a2c1d71a0db245
jasser boulila <jasserboulila@proton.me>
2026-01-26 16:12:06 +0100

    added all of the planet radiuses

---
+++ lab3.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Lab2SpaceExploration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // add all the planet radiuses
+            
+            double mercuryRadius = 2439.7;
+            double venusRadius = 6051.8;
+            double earthRadius = 6371.0;
+            double marsRadius = 3389.5;
+            double jupiterRadius = 69911.0;
+            double saturnRadius = 58232.0;
+            double uranusRadius = 25362.0;
+            double neptuneRadius = 24622.0;
+
+        }
+    }
+}
