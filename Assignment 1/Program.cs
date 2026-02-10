aa7254fddb681053bbd7608542a8ec33ac097b4f
jasser boulila <jasserboulila@proton.me>
2026-02-09 18:33:50 -0600

    created the step 2 to create a variable for the user input

--- Assignment 1/Program.cs
+++ Assignment 1/Program.cs
@@ -6,7 +6,14 @@ public static class Assignment1
     
     public static void Run()
     {
-        
+        // three strings to store the price of each type of flooring
+        string hardwood = "8.95";
+        string carpet = "7.95";
+        string laminate = "6.95";
+
+
+        // variable to store the user choice
+        string userChoice = Console.ReadLine();
         
     }
 }
