6330a5c81687389d0e989080e2a9bd5f6abf34a0
jasser boulila <jasserboulila@proton.me>
2026-03-03 20:59:18 -0600

    step2

--- Lab6/Lab6.cs
+++ Lab6/Lab6.cs
@@ -11,6 +11,7 @@ public static class Assignment6
         // STEP 1: Declare a new array (of type string), and initialize it with anywhere from 7 to 9 different ice cream flavors;
         string[] iceCreamFlavors = new string[] { "Vanilla", "Chocolate", "Strawberry", "Pistachio", "Mint", "Raspberry", "Cookie Dough", "Blueberry", "Peach" };   
         // STEP 2: Create a variable to contain the Length property of the array you created in STEP 1
+        int length = iceCreamFlavors.Length;
         // STEP 3: Output a simple sentence to the Console indicating to the user that you are about to list all the ice cream flavors available at your stand using a FOR() loop
         // STEP 4: Build a FOR() loop to iterate through the array you created in STEP 1, using the variable you created in STEP 2 to control how many times the loop repeats
         // STEP 5: Inside the FOR() loop, output each element of the array to the Console
