15a0a1ce5165f2303022bd800c7852ccdbed90c1
jasser boulila <jasserboulila@proton.me>
2026-03-22 14:29:23 -0400

    created the array to store the user guesses

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -6,6 +6,11 @@ class Assignment2
     {
         // step1 : create the random variables
         Random random = new Random();
-        int targetTemperature = random.Next(10, 41); // 10 to 40 
+        int targetTemperature = random.Next(10, 41); // 10 to 40
+
+        // step 2 : created the array to store the user guesses
+        int[] guesses = new int[100]; 
+        int index = 0;
+        int userGuess = 0; 
     }
 }
