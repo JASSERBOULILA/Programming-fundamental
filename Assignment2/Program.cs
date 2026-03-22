3be10052650edbf2eccdf9a2ac1eb593eb2240c3
jasser boulila <jasserboulila@proton.me>
2026-03-22 14:27:18 -0400

    initialized the program.cs file

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Assignment2
+{
+    static void Main()
+    {
+        Console.WriteLine("Temperature Game Starting...");
+    }
+}
