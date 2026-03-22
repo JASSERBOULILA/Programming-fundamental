eb1a3008a04a988739f99e452df6b5bacf27942e
jasser boulila <jasserboulila@proton.me>
2026-03-22 14:36:18 -0400

    created the if statment to check the user if it match the target temp and if the user didnt give a valid number error will show

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 class Assignment2
 {
@@ -22,6 +23,33 @@ class Assignment2
             // STEP 3  : ask the user to enter their guess
             Console.Write("Enter your guess: ");
             string input = Console.ReadLine();
+            // Step 4 : check if the input is a valid number
+            if(int.TryParse(input , out userGuess))
+            {
+                
+                guesses[index] = userGuess;
+                index++;
+                // step 5 : did the if statement to check the user guess if it above the target temperature console too warm message if it is below the target temperature console too cold message if it is equal to the target temperature console correct message
+                if(userGuess > targetTemperature)
+                {
+                    
+                    Console.WriteLine("Your guess is too Warm.");
+                }else if(userGuess < targetTemperature)
+                {
+                    Console.WriteLine("Your guess is too Cold.");
+                }else
+                {
+                    Console.WriteLine("Your guess is correct!");
+                    break;
+                }
+
+            }
+            // this else statement if the user typed a number that is not a valid number like characters or symboles it will print this error
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+
+            }
 
         }
     }
