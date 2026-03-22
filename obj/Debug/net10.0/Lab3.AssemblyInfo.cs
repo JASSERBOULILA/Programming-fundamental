c199239d4d9ff8f5ab66124615205872d77d32d4
jasser boulila <jasserboulila@proton.me>
2026-03-22 14:26:46 -0400

    created the assignment 2 project

--- obj/Debug/net10.0/Lab3.AssemblyInfo.cs
+++ obj/Debug/net10.0/Lab3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8202398bb7a5e7ed467ef822c132dc4ff18ebb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d01c0b9278ec8f805eaf5fdc50ff558415692448")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
