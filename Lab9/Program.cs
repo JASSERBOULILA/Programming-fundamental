4820209f5aaf7b46d45952936eba14e6eb2867fa
jasser boulila <jasserboulila@proton.me>
2026-03-22 23:37:42 -0400

    step 9 ,10,11,12 created the instance of class ball and callit ball1 and we called the method description using the the ball1 object instance and created another instance called it ball2 and we called the description method again

--- Lab9/Program.cs
+++ Lab9/Program.cs
@@ -1,6 +1,24 @@
 ﻿// STEP 1: Add a dependency with the 'using' directive - we will call the namespace 'Lab9'
 using Lab9;
 using System;
+
+class LabProgram
+{
+    static void Main(string[] args)
+    {
+        // STEP 9: Create an instance of class Ball and call it 'ball1' - it will be red, 10 cm in diameter, and rubber
+        Ball ball1 = new Ball("Red", 10, "Rubber");
+
+        // STEP 10: Call the Description method using the ball1 object instance
+        ball1.Description();
+
+        // STEP 11: Create another instance of a Ball object, calling it 'ball2' - you will choose the 3 arguments
+        Ball ball2 = new Ball("Blue", 20, "Plastic");
+
+        // STEP 12: Call the Description method using the ball2 object instance
+        ball2.Description();
+    }
+}
 /*
 Lab 9
 This lab will give you more practice building a class, then creating an instance of that class (an object)
@@ -13,8 +31,4 @@ Then you will create a method for the class that will output a simple descriptio
 
 // STEP 2: We will add a new class in a separate file - go to Project > Add Class... in Visual Studio and create a new class called Lab9.cs
 // STEP 3: Copy the code for Lab9.cs (just below) and paste it into the new file - proceed to that file for STEP 4
-// STEP 9: Create an instance of class Ball and call it 'ball1' - it will be red, 10 cm in diameter, and rubber
-// STEP 10: Call the Description method using the ball1 object instance
-// STEP 11: Create another instance of a Ball object, calling it 'ball2' - you will choose the 3 arguments
-// STEP 12: Call the Description method using the ball2 object instance
 // STEP 13: Paste the code for both Program.cs and Lab9.cs into the submission text area below. Please use the "Code snippet" (</>) button so that your code formatting is retained.
