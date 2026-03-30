74842075c357359621faacd650172b225549e0c3
jasser boulila <jasserboulila@proton.me>
2026-03-29 21:46:08 -0400

    initialized the lab 10 assignment

--- Lab9/Program.cs
+++ Lab9/Program.cs
@@ -1,7 +1,20 @@
-﻿// STEP 1: Add a dependency with the 'using' directive - we will call the namespace 'Lab9'
+﻿/*
+Lab 9
+This lab will give you more practice building a class, then creating an instance of that class (an object)
+Your job is to create a Class for a ball object. The class will feature the following properties:
+1. Color
+2. Size (diameter)
+3. Material
+Then you will create a method for the class that will output a simple description of the ball object to the console
+*/
+
+
+// STEP 1: Add a dependency with the 'using' directive - we will call the namespace 'Lab9'
 using Lab9;
 using System;
 
+// STEP 2: We will add a new class in a separate file - go to Project > Add Class... in Visual Studio and create a new class called Lab9.cs
+// STEP 3: Copy the code for Lab9.cs (just below) and paste it into the new file - proceed to that file for STEP 4
 class LabProgram
 {
     static void Main(string[] args)
@@ -19,16 +32,5 @@ class LabProgram
         ball2.Description();
     }
 }
-/*
-Lab 9
-This lab will give you more practice building a class, then creating an instance of that class (an object)
-Your job is to create a Class for a ball object. The class will feature the following properties:
-1. Color
-2. Size (diameter)
-3. Material
-Then you will create a method for the class that will output a simple description of the ball object to the console
-*/
 
-// STEP 2: We will add a new class in a separate file - go to Project > Add Class... in Visual Studio and create a new class called Lab9.cs
-// STEP 3: Copy the code for Lab9.cs (just below) and paste it into the new file - proceed to that file for STEP 4
 // STEP 13: Paste the code for both Program.cs and Lab9.cs into the submission text area below. Please use the "Code snippet" (</>) button so that your code formatting is retained.
--- obj/Debug/net10.0/Lab3.AssemblyInfo.cs
+++ obj/Debug/net10.0/Lab3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a522cb30be0d32cc11b4991dc4f813b4ca5f8e93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4820209f5aaf7b46d45952936eba14e6eb2867fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
