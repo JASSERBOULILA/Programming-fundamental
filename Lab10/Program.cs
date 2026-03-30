b8aeaedcd22b5874e53a2bfddd4c20445bbeef28
jasser boulila <jasserboulila@proton.me>
2026-03-29 21:48:12 -0400

    done the step 1 &2

--- Lab10/Program.cs
+++ Lab10/Program.cs
@@ -1,24 +1,26 @@
 ﻿// LAB 10
 // STEP 1: Declare "Shapes" as a dependency with "using"
-﻿
+using Shapes;
+
 // STEP 2: Proceed to Shapes.cs and examine the class Rectangle
-﻿
+
 // STEP 3: Create an instance of a new Rectangle object (call it var rectangle1) and give it a height and width in integers (passed in as arguments)
-﻿
+
 // STEP 4a: Get a description of the rectangle1 object by invoking (or calling) the DescribeRectangle() method and assigning it to a string type called description1
-﻿
+
 // STEP 4b: Output description1 using Console.WriteLine()
-﻿
+
 // STEP 4c: Draw the rectangle1 object to the console by invoking the DrawRectangle() method
-﻿
+
 // STEP 5: Compile and run the application to see the result or to track down any errors
 ﻿
+
 // STEP 6: Create another instance of a Rectangle object (var rectangle2) and give it a different height and width in integers (as arguments)
-﻿
+
 // STEP 7a: Get a description of the rectangle object by invoking (calling) the DescribeRectangle() method and assigning it to a string type called description1
-﻿
+
 // STEP 7b: Output description1 using Console.WriteLine()
-﻿
+
 // STEP 7c: Draw the rectangle1 object to the console by invoking the DrawRectangle() method
-﻿
+
 // STEP 8: Compile and run the application once again to see the result
